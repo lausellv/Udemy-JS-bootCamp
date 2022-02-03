6d21cf3cb11133c302af5467ce0463acaca14b5f
Victor <lausellv@yahoo.com>
2022-02-03 21:03:31 +0100

    debounce practice

--- movies-229-debouce/utils.js
+++ movies-229-debouce/utils.js
@@ -8,9 +8,27 @@ const debounce = (func, delay = 1000) => {
   };
 };
 
-
 // you pass in a function (func) and it returns a function (function is a reserved word in JS)
 const debounce2 = (func, delay) => {
+  let timeoutId;
+  return function (...args) {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const debounce3 = (func, delay = 1000) => {
+  let timeoutId;
+  return function (...args) {
+    timeoutId = setTimeout(() => {
+      func.apply(null, args);
+    }, delay);
+  };
+};
+
+const debounce4 = (func, delay = 1000) => {
   let timeoutId;
   return function (...args) {
     if (timeoutId) clearTimeout(timeoutId);
